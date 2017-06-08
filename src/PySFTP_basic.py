0abe98e2d1aa135d93376e8bf2ec961791d7d44a
Fernando Dantas <development@dantas.me>
2017-06-07 22:20:03 -0300

    Pysftp basics

---
+++ src/PySFTP_basic.py
@@ -0,0 +1,35 @@
+'''
+Created on Jun 6, 2017
+
+@author: makaidaimyo
+'''
+
+'''
+pip install pycrypto
+pip install paramiko
+pip install pysftp
+'''
+import pysftp as sftp
+
+def push_file_to_server():
+    s = sftp.Connection(host='138.197.151.242',
+                        username='root',
+                        password='')
+    local_path = "test_file.txt"
+    remote_path = "/home/test_file.txt"
+    
+    s.put(local_path, remote_path)
+    s.close()
+    
+#push_file_to_server()
+
+def get_file_from_server():
+    s = sftp.Connection(host='138.197.151.242',
+                        username='root',
+                        password='')
+    local_path = "test_file.txt"
+    remote_path = "/home/test_file.txt"
+    s.get(local_path, remote_path)
+    s.close()
+    
+get_file_from_server()
--- src/urllib_basic.py
+++ src/urllib_basic.py
@@ -30,21 +30,22 @@ print(resp_data)
 
 from bs4 import BeautifulSoup as BSoup
 import urllib.request as url_req
-#import urllib.parse as url_par
+import time
 
 req = url_req.urlopen('https://www.engadget.com/rss.xml')
 
 xml = BSoup(req, 'xml')
 
 for item in xml.findAll('item'):
-    #print(item)
-    for title in item.findAll('title'):
-        print(title)
-        for link in item.findAll('link'):
-            print(link.text)
-    #url = item.text
-    #news = url_req.urlopen(url).read()
-    #print(news)
-    #print(20*"#")
+    print(item.title)
+    print(item.link)
+    
+    news = url_req.urlopen(item.link.text).read()
+    page = BSoup(news, 'lxml')
+    for p in page.findAll('p'):
+        print(p.text + "\n")
+    time.sleep(2)
+                
+
     
     
