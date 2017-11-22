2efc273846e4c2dfd12093afef3597330b2b977a
fernando <fernando@192.168.1.100>
2017-11-21 22:14:01 -0700

    Finished Stone River course - Python Web Programming

--- src/FlaskBasics/www/FlaskApp/FlaskApp/__init__.py
+++ src/FlaskBasics/www/FlaskApp/FlaskApp/__init__.py
@@ -10,6 +10,26 @@ def homepage():
         return render_template('index.html', name = name, example_list = example_list)
     except Exception as e:
         return(str(e))
+
+@app.route("/page-one/")
+def pageone():
+    try:
+        name = "Page One"
+        return render_template('page-one.html', name = name)
+    except Exception as e:
+        return(str(e))
+    
+@app.route("/about/")
+def aboutepage():
+    try:
+        name = "About"
+        return render_template('index.html', name = name)
+    except Exception as e:
+        return(str(e))
+        
+@app.errorhandler(404)
+def four_oh_four(e):
+    return render_template('404.html')
         
 if __name__ == "__main__":
     app.run()
